518701f447c33dc959dd3d66c559b898f99572ad
Jaycarlx <molokwuchibuzorchristian@gmail.com>
2022-06-21 22:58:09 +0100

    Intro to JS

---
+++ intro.js
@@ -0,0 +1,8 @@
+alert("Name:Molokwu Chibuzor");
+alert("Height: 5'7");
+alert("Country:Nigeria");
+
+//Code block to print on screen incase alert isnt accepted as instructions was not clear
+document.write("Name:Molokwu Chibuzor");
+document.write("Height: 5'7");
+document.write("Country: Nigeria");
